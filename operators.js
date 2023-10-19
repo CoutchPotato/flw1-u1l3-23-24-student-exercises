8b7e53c685ec59f3d4af3b2fb0fb52cedf3fc5b1
CoutchPotato <140278466+CoutchPotato@users.noreply.github.com>
2023-10-19 23:26:57 +0000

    FLW Oct 19

--- operators.js
+++ operators.js
@@ -1,28 +1,31 @@
 // Arithmetic Operations
-
+let num1 = 13
+let num2 = 21
 
 // 1. Addition
 // Create two variables, num1 and num2, and assign them any two numbers of your choice.
 // Add the two numbers and store the result in a new variable called sum.
 // Print the result using console.log().
-
-
-
+let add = num1 + num2;
+console.log(add);
 // 2. Subtraction
 // Subtract num2 from num1 and store the result in a new variable called difference.
 // Print the result.
-
+let sub = num1 - num2;
+console.log(sub);
 
 // 3. Multiplication
 // Multiply num1 by num2 and store the result in a new variable called product.
 // Print the result.
-
-
-
+let multi = num1 * num2;
+console.log(multi);
 // String Operations
 
 // 4. Concatenation with +
 // Create two string variables, str1 and str2, and assign them any two strings of your choice.
 // Concatenate (combine) the two strings using the + operator and store the result in a new variable called combinedString.
 // Print the result.
-
+let str1 = "Hello";
+let str2 = "World";
+let together = str1 + " " + str2;
+console.log(together);
